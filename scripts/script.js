34f8016e8ce5da4d12a9ae6a39524973a91e8a33
Pammachius Jérôme <scipions.prof@free.fr>
2023-08-23 00:17:22 +0200

    fin du 3e chapitre

--- scripts/script.js
+++ scripts/script.js
@@ -29,15 +29,31 @@ function lancerJeu() {
     let nombreQuestions = 0
     let boutonvalider = document.getElementById("btnValiderMot")
     let inputEcriture = document.getElementById("inputEcriture")
+    let i = 0
+    afficherProposition(listeMots[i])
     boutonvalider.addEventListener("click", () => {
         console.log(inputEcriture.value)
-   
+        if (inputEcriture.value === listeMots[i]) {
+            score++
+        }
+        
+        i++
+        afficherresultat(score, i)
+        inputEcriture.value = ""
+        if (listeMots[i] === undefined) {
+            afficherProposition("le jeu est fini")
+            boutonvalider.Disabled =true
+        } else {
+            afficherProposition(listeMots[i])    
+        }
+       
     }) 
     
-    
-    
-    
-    afficherresultat(score, nombreQuestions)
+    afficherresultat(score, i)
 }
 
-
+// affiche le mot dans la zone de proposition
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerHTML = proposition
+}
